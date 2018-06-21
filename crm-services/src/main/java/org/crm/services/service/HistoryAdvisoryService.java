bf52c3c36a99882b3812a468471ff292080f75e5
minhnhat <nhattvm11@gmail.com>
2018-06-21 10:08:23 +0700

    commit

---
+++ crm-services/src/main/java/org/crm/services/service/HistoryAdvisoryService.java
@@ -0,0 +1,12 @@
+package org.crm.services.service;
+
+import com.smartdev.user.entity.HistoryAdvisory;
+import com.smartdev.user.model.StatusCount;
+
+import java.util.List;
+
+public interface HistoryAdvisoryService {
+    List<HistoryAdvisory> findAll();
+
+    StatusCount getNumberOfEachStatus();
+}
---
+++ crm-services/src/main/java/org/crm/services/service/HistoryAdvisoryServiceImpl.java
@@ -0,0 +1,51 @@
+package org.crm.services.service;
+
+import com.smartdev.user.dao.repository.HistoryAdvisoryRepository;
+import com.smartdev.user.entity.HistoryAdvisory;
+import com.smartdev.user.model.StatusCount;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class HistoryAdvisoryServiceImpl implements HistoryAdvisoryService {
+
+    public static final String PROSPECT = "prospect";
+    public static final String LEAD = "lead";
+    public static final String POTENTIAL_LEAD = "potential lead";
+    public static final String ACTIVE_LEAD = "active lead";
+
+    @Autowired
+    private HistoryAdvisoryRepository historyAdvisoryRepository;
+
+    @Override
+    public List<HistoryAdvisory> findAll() {
+        return historyAdvisoryRepository.findAll();
+    }
+
+    @Override
+    public StatusCount getNumberOfEachStatus() {
+        List<HistoryAdvisory> historyAdvisories = findAll();
+        if(historyAdvisories.isEmpty())
+            return new StatusCount(0,0,0,0);
+        StatusCount statusCount = new StatusCount();
+        for (HistoryAdvisory h : historyAdvisories) {
+            switch (h.getStatusByStatusId().getName()) {
+                case PROSPECT:
+                    statusCount.setProspect(statusCount.getProspect() + 1);
+                    break;
+                case LEAD:
+                    statusCount.setLead(statusCount.getLead() + 1);
+                    break;
+                case POTENTIAL_LEAD:
+                    statusCount.setPotentialLead(statusCount.getPotentialLead() + 1);
+                    break;
+                case ACTIVE_LEAD:
+                    statusCount.setActiveLead(statusCount.getActiveLead() + 1);
+                    break;
+            }
+        }
+        return statusCount;
+    }
+}
