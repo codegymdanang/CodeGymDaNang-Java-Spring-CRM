8ec261ad7e041d453b6fbbf5d0c05c9446e7d4ed
PC <minhtrinhnguyendtu@gmail.com>
2018-06-26 10:56:33 +0700

    list-customer

---
+++ crm-services/src/main/java/org/crm/services/service/ListCustomManageService.java
@@ -0,0 +1,9 @@
+package org.crm.services.service;
+
+import com.smartdev.user.entity.Customer;
+
+import java.util.List;
+
+public interface ListCustomManageService {
+   public List<Customer> listAllCustomer();
+}
