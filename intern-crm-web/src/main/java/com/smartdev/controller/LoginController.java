f3a84c87329082e55e6819974ac6be9062510e72
Hanv2itedu <hanv2.itedu@gmail.com>
2018-06-28 16:33:48 +0700

    add authenticate relogin

--- intern-crm-web/src/main/java/com/smartdev/controller/LoginController.java
+++ intern-crm-web/src/main/java/com/smartdev/controller/LoginController.java
@@ -1,28 +1,50 @@
 package com.smartdev.controller;
 
 import com.smartdev.crm.service.user.UserService;
-import com.smartdev.user.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.sql.SQLException;
+import javax.management.relation.Role;
+import java.util.Collection;
+import java.util.Set;
 
 @Controller
 public class LoginController {
     @Autowired
     UserService userService;
 
-    @RequestMapping(value="/", method = RequestMethod.GET)
-    public String home(){
-//        User user = new User("seller1", "seller1", 0);
-//        userService.save(user, "SELLER");
-//        System.out.printf("save successfully" + user.getUserName().toString());
-        return "login";
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
+        boolean isSeller = false;
+        boolean isManager = false;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Collection<? extends GrantedAuthority> authorities
+                = authentication.getAuthorities();
+        for (GrantedAuthority grantedAuthority : authorities) {
+            if (grantedAuthority.getAuthority().equals("ROLE_SELLER")) {
+                isSeller = true;
+            } else if (grantedAuthority.getAuthority().equals("ROLE_MANAGER")) {
+                isManager = true;
+            }
+        }
+        if (isManager == true) {
+            return "redirect:/manager_crm/home";
+        } else if (isSeller) {
+            return "redirect:/seller/list-custom-seller";
+        } else {
+            return "login";
+        }
+
     }
-    @RequestMapping(value="/login", method = RequestMethod.POST)
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
     public String login(Model model, String error, String logout) {
         if (error != null)
             model.addAttribute("error", "Your username and password is invalid.");
@@ -30,8 +52,9 @@ public class LoginController {
             model.addAttribute("message", "You have been logged out successfully.");
         return "login";
     }
-    @RequestMapping(value="/logout", method = RequestMethod.GET)
-    public String logout(){
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logout() {
         return "login";
     }
 }
